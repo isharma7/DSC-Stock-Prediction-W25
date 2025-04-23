ae134b236391bf8d77fc510551cdae5565c09c94
czhong06 <clarazhong306@gmail.com>
2025-04-23 16:07:08 -0700

    moving average function

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import os
 
+def moving_average(filepath, window):
+    #read csv
+    df = pd.read_csv(filepath, skiprows=2, parse_dates=['Date'])
+
+    #sort by date
+    df = df.sort_values('Date')
+    
+    #calculate 7-day moving average
+    df['MA'] = df['Unnamed: 2'].rolling(window).mean()
+
+    return df
+df = moving_average('data/AAPL.csv', 10)
+print(df[['Date', 'Unnamed: 2', 'MA']].tail())
+    
