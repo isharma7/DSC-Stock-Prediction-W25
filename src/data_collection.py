50ecd21af33be9cd7828be7285b948261484c66e
kingtemple <rohit1.krishna1@gmail.com>
2025-04-24 20:28:41 -0700

    This is a line graph that shows apple closing prices

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv('../data/AAPL.csv')
+
+print(df.head())
+
+df['Date'] = pd.to_datetime(df['Date'])
+
+plt.figure(figsize=(10, 6))
+plt.plot(df['Date'], df['Close'], label='AAPL Closing Price', color='blue')
+plt.title('AAPL Closing Prices As Time Goes')
+plt.xlabel('Date')
+plt.ylabel('Prices ($)')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+# Creates a small line graph that shows the closing prices
